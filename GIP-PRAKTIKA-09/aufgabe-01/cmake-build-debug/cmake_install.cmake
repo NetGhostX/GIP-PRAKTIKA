43660d16af42023bc7c5e65183640d78bf513180
low-profil <115982245+low-profil@users.noreply.github.com>
2022-12-07 00:46:16 +0100

    initial commit

--- GIP-PRAKTIKA-09/aufgabe-01/cmake-build-debug/cmake_install.cmake
+++ GIP-PRAKTIKA-09/aufgabe-01/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Kelvin/Documents/GitHub/GIP-PRAKTIKA/GIP-PRAKTIKA-09/aufgabe-01
+# Install script for directory: C:/Users/kelvi/Documents/GitHub/GIP-PRAKTIKA/GIP-PRAKTIKA-09-TEST/aufgabe-01
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Users/Kelvin/AppData/Local/JetBrains/CLion 2022.2.4/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2022.2.4/bin/mingw/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Kelvin/Documents/GitHub/GIP-PRAKTIKA/GIP-PRAKTIKA-09/aufgabe-01/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/kelvi/Documents/GitHub/GIP-PRAKTIKA/GIP-PRAKTIKA-09-TEST/aufgabe-01/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
